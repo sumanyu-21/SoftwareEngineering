0c735d148eec075e5af660ec71f039d2f3683799
sumanyu-21 <sumanyugarglsp@gmail.com>
2020-10-19 17:02:07 +0530

    Update Advance Implementation Details.cpp

--- STL/Advance Implementation Details.cpp
+++ STL/Advance Implementation Details.cpp
@@ -26,6 +26,18 @@ namespace std
 	};
 }
 
+/***************************************************************************************************/
+// A hash function used to hash a pair of any kind in unordered_map
+struct hash_pair { 
+    template <class T1, class T2> 
+    size_t operator()(const pair<T1, T2>& p) const
+    { 
+        auto hash1 = hash<T1>{}(p.first); 
+        auto hash2 = hash<T2>{}(p.second); 
+        return hash1 ^ hash2; 
+    } 
+}; 
+unordered_map<pair<int,int>, int, hash_pair>
 
 /****************************PRIORITY_QUEUE/SET/MULTISET CUSTOM ORDERING**********************************/
 /*IMPORTANT POINTS--PRIORITY QUEUE ->USE GREATER THAN SIGN FOR MIN HEAP AND VICE VERSA
